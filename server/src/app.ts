4988352e3fba0ba5369430a38602d34e23cb85c3
Anis <anisboudaren2@gmail.com>
2025-05-27 19:16:15 +0100

    change origin

--- server/src/app.ts
+++ server/src/app.ts
@@ -11,18 +11,27 @@ import cookieParser from "cookie-parser";
 
 dotenv.config();
 import "./auth/passport";
-
+const isProduction = process.env.NODE_ENV === "production"
 const app = express();
 
+const allowedOrigins = [
+  'https://coucou-client.vercel.app',
+  'http://localhost:3000'
+];
+
 app.use(cors({
-     origin: [
-    'https://coucou-client.vercel.app',
-    'http://localhost:3000' // local frontend
-  ],
-    methods: ['GET', 'POST', 'OPTIONS' , 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true
-  }));
+  origin: function (origin, callback) {
+    // Allow requests with no origin like curl or Postman
+    if (!origin || allowedOrigins.includes(origin)) {
+       callback(null, true);
+    }
+     callback(new Error('Not allowed by CORS'));
+  },
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}));
+
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cookieParser());
